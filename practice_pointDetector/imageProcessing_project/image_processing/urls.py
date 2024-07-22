03a88798ce3878685e0d717a5da1dc89af60ec42
Alexey <95855844+TwentyOn@users.noreply.github.com>
2024-07-22 14:30:44 +0500

    Add files via upload

--- practice_pointDetector/imageProcessing_project/image_processing/urls.py
+++ practice_pointDetector/imageProcessing_project/image_processing/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from .views import ImageViewSet
 
 urlpatterns = [
-    path('images/', ImageViewSet.as_view()),
+    path('api/token/images/', ImageViewSet.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
--- practice_pointDetector/imageProcessing_project/image_processing/views.py
+++ practice_pointDetector/imageProcessing_project/image_processing/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Image
 from .serializers import ImageSerializer
@@ -14,12 +15,10 @@ from rest_framework import generics
 from django.http import HttpResponse
 
 
-class ImageViewSet(APIView):
-    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-    def create_auth_token(sender, instance=None, created=False, **kwargs):
-        if created:
-            Token.objects.create(user=instance)
-
+class ImageViewSet(generics.RetrieveAPIView):
+    queryset = Image.objects.all()
+    serializer = ImageSerializer
+    permission_classes = (IsAuthenticated, )
     def get(self, request, *args, **kwargs):
         images = Image.objects.all()
         serializer = ImageSerializer(images, context={'request': request}, many=True)
